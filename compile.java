5f01cded29e2e65e4ccf0b6a7247cd87ce25efb2
rohit <rohit@kali.kali>
2024-08-28 14:13:23 +0530

    final

--- compile.java
+++ compile.java
@@ -1,15 +1,1 @@
-class compile {
-public static int print10(){
-int i=0;
-do {
-System.out.println("anything else  "+i);
-i++;
-}
-while(i<=50);
-return 400;
-}
-public static void main(String args[]){
-
-System.out.println(print10());
-
-}}
+class compile {public static void main(String args[]){System.out.println("hello world");}}
